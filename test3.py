120cc750c3418513dc7ed24933838b211f30a9d3
Kavya-Kapa <82310415+Kavya-Kapa@users.noreply.github.com>
2021-04-23 16:15:02 +0530

    characters frequency

---
+++ test3.py
@@ -0,0 +1,12 @@
+#characters frequency in a string
+n=input('Please enter a string ')
+def most_frequent(string):
+    d = dict()
+    for i in string:
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] += 1
+    return d
+
+print (most_frequent(n))
